c009a3cf9940cf230224b04582a00bc35ef27c34
Jahir xd <yepezjahir@gmail.com>
2025-12-09 23:11:48 -0600

    Routes de Screens y MainActivity corregido

---
+++ app/src/main/java/com/example/proyectofinal/ui/screens/PostDetailScreen.kt
@@ -0,0 +1,17 @@
+
+package com.example.proyectofinal.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.proyectofinal.ui.viewmodels.PostViewModel
+
+@Composable
+fun PostDetailScreen(
+    navController: NavHostController,
+    postId: Int,
+    postViewModel: PostViewModel = viewModel()
+) {
+
+}
---
+++ app/src/main/java/com/example/proyectofinal/ui/screens/RegisterScreen.kt
@@ -0,0 +1,15 @@
+package com.example.proyectofinal.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.proyectofinal.ui.viewmodels.AuthViewModel
+
+@Composable
+fun RegisterScreen(
+    navController: NavHostController,
+    authViewModel: AuthViewModel = viewModel()
+){
+
+}
